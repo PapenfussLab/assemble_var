a9c9b53f08bb955716c6b505c6ba91908dc2b1d0
Administrator <admin@local.host>
2015-07-29 14:07:06 +1000

    fix again

--- assemble_var/third_party_runners.py
+++ assemble_var/third_party_runners.py
@@ -1,20 +1,10 @@
 import os, sys, inspect
 from subprocess import check_call
 
-# realpath() will make your script run, even if you symlink it :)
-cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( 
-    inspect.currentframe() ))[0]))
-if cmd_folder not in sys.path:
-    sys.path.insert(0, cmd_folder)
-
- # use this if you want to include modules from a subfolder
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
-    inspect.getfile( inspect.currentframe() ))[0],"/third-party/")))
-if cmd_subfolder not in sys.path:
-    sys.path.insert(0, cmd_subfolder)
-
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
-    inspect.getfile( inspect.currentframe() ))[0],"/third-party/mungo/")))
+# use this if you want to include modules from a subfolder
+cmd_subfolder = (os.path.dirname(os.path.dirname(os.path.realpath(__file__))) 
+    + "/third-party/")
+print cmd_subfolder
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
 
