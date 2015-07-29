97dd8f368a2d5e671595a5094b2fcc331809e727
Administrator <admin@local.host>
2015-07-29 14:02:28 +1000

    little fix

--- assemble_var/third_party_runners.py
+++ assemble_var/third_party_runners.py
@@ -1,6 +1,23 @@
-import sys
+import os, sys, inspect
 from subprocess import check_call
-import os
+
+# realpath() will make your script run, even if you symlink it :)
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( 
+    inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+ # use this if you want to include modules from a subfolder
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
+    inspect.getfile( inspect.currentframe() ))[0],"/third-party/")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
+    inspect.getfile( inspect.currentframe() ))[0],"/third-party/mungo/")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+
 import re
 import glob
 from mungo.fasta import FastaReader
