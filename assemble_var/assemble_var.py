604a6b1af1280d5e41fd1fd52892316d29e23407
Administrator <admin@local.host>
2015-07-29 13:15:18 +1000

    little fix

--- assemble_var/assemble_var.py
+++ assemble_var/assemble_var.py
@@ -2,17 +2,18 @@ from optparse import OptionParser
 import third_party_runners as thrd
 import os, sys
 import inspect
+
  # realpath() will make your script run, even if you symlink it :)
- cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( 
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( 
     inspect.currentframe() ))[0]))
- if cmd_folder not in sys.path:
-     sys.path.insert(0, cmd_folder)
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
 
  # use this if you want to include modules from a subfolder
- cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
     inspect.getfile( inspect.currentframe() ))[0],"/third_party/")))
- if cmd_subfolder not in sys.path:
-     sys.path.insert(0, cmd_subfolder)
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
 
 
 def build(options):
